4f2ed91d6c380d44b273e92867aac7672f31dfb2
Mariusz12345 <przybylowski303@gmail.com>
2015-08-14 14:02:38 +0200

    GWTPlatform

---
+++ GWTPlatform/src/main/java/GWTPlatform/projekcik/client/place/NameTokens.java
@@ -0,0 +1,9 @@
+package GWTPlatform.projekcik.client.place;
+
+public class NameTokens {
+    public static final String home = "!home";
+
+    public static String getHome() {
+        return home;
+    }
+}
